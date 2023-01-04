f4b0aecf71b39d6a93efb42dd4d7731f027fd385
newo-2001 <o.elderbroek@gmail.com>
2023-01-04 21:47:06 +0100

    Expose GET product endpoints to unauthenticated users

--- src/main/java/me/new2001/webshop/security/SecurityConfiguration.java
+++ src/main/java/me/new2001/webshop/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import me.new2001.webshop.common.ExceptionHandlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -46,6 +47,7 @@ public class SecurityConfiguration {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**", "/error").permitAll()
+                .antMatchers(HttpMethod.GET, "/products/**").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
